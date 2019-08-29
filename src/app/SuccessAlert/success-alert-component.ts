ae441837099a153b195f9566738337b34edfc347
ChrisMcEwen <cmcewen87@gmail.com>
2019-08-29 14:14:11 -0400

    First commit

---
+++ src/app/SuccessAlert/success-alert-component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'SuccessAlert',
+  template: `
+  <div class="alert alert-success">
+  <strong>Success!</strong>&nbsp; I am really enjoying this course on Angular!
+  </div>
+  `
+})
+
+export class SuccessAlert {
+
+}
---
+++ src/app/WarningAlert/warning-alert-component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'WarningAlert',
+  template: `
+  <div class="alert alert-danger">
+  &nbsp;<strong>Warning!</strong>&nbsp; I said \"I\'m enjoying this course\"... why the warning?
+  </div>
+  `
+})
+
+export class WarningAlert {
+
+}
